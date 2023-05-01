f2131457f57a23e75106ad45dc899dad67bfdfc2
Oliver Schmidt <oliverpierre.schmidt@gmail.com>
2023-05-01 17:48:39 +0200

    add cli options

--- src/BlazingClassDiagram/BlazingClassDiagram/Program.cs
+++ src/BlazingClassDiagram/BlazingClassDiagram/Program.cs
@@ -3,8 +3,26 @@
 using BlazingClassDiagram.Mermaid;
 using BlazingClassDiagram.Models;
 using BlazingClassDiagram.Syntax;
+using CommandLine.Text;
+using CommandLine;
 
-var content = File.ReadAllText(@"C:\work\GitHub\CloudAdoptionFramework\ready\AzNamingTool\Models\PolicyDefinition\PolicyRule.cs");
-var root = new Root();
-root.Parse(content);
-var mermaid = Renderer.Render(root);
+CommandLine.Parser.Default.ParseArguments<Options>(args)
+    .WithParsed<Options>(o =>
+    {
+        var content = File.ReadAllText(@"C:\work\GitHub\CloudAdoptionFramework\ready\AzNamingTool\Models\PolicyDefinition\PolicyRule.cs");
+        var root = new Root();
+        root.Parse(content);
+        var mermaid = Renderer.Render(root);
+    });
+
+
+public class Options
+{
+    [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+    public bool Verbose { get; set; }
+
+    [Option('p', "path", Required = false, HelpText = "Set input path. (can be .cs file or an directory)")]
+    public string Path { get; set; }
+
+
+}
